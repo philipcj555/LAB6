4eb28a1b1071690c35765e2f4d8b4f87fb0db2e3
philipjoseph <philipcj555@gmail.com>
2023-10-25 14:04:30 -0400

    u

---
+++ main.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def encode(password):
+    encoded_pass = ""
+    for i in password:
+        i = int(i)
+        t = i + 1
+        u = str(t)
+        encoded_pass += u
+    return encoded_pass
+
+
+
+def decode(password):
+    decodedpass = ""
+    for i in password:
+        i = int (1)
+        p = i -3
+        o = str(p)
+        decodedpass += o
+    return decodedpass
+
+def main():
+    encoded = None
+
+    while True:
+        print("Menu")
+        print("--------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        menu_selection = int(input("please enter an option: "))
+        if menu_selection == 1:
+            password = input("Please enter your password to encode: ")
+            encoded = encode(password)
+
+        elif menu_selection ==2:
+            decoded = decode(encoded)
+            print("The encoded password is " + encoded + "and the orginal password is " + decoded + ".")
+        elif menu_selection ==3:
+            break
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
